d9bd29c2defb7635d71fcbe0f50dc5a4d778fcd4
Knut Forkalsrud <knut@forkalsrud.org>
2017-05-22 02:00:20 -0700

    Fix test

--- mysqlfs/src/test/java/org/forkalsrud/mysqlfs/ReferenceTest.java
+++ mysqlfs/src/test/java/org/forkalsrud/mysqlfs/ReferenceTest.java
@@ -2,10 +2,12 @@ package org.forkalsrud.mysqlfs;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 
@@ -20,8 +22,12 @@ public class ReferenceTest {
     @Test
     public void testGetAttributesForNonExistentFile() throws IOException {
         
-        Path p = new File(System.getProperty("user.home"), "nonexistent").toPath();
-        BasicFileAttributes attrs = Files.readAttributes(p, BasicFileAttributes.class);
-        assertFalse(attrs.isDirectory());
+        try {
+            Path p = new File(System.getProperty("user.home"), "nonexistent").toPath();
+            Files.readAttributes(p, BasicFileAttributes.class);
+            fail("Should not exist");
+        } catch (Exception e) {
+            assertEquals(NoSuchFileException.class, e.getClass());
+        }
     }
 }
